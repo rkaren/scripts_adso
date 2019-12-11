4c007b20a1ddca4b70e73a4bc32e833d01d50004
rkaren <55139942+rkaren@users.noreply.github.com>
2019-12-11 22:39:23 +0100

    Add files via upload

---
+++ ejercicio611ps1.ps1
@@ -0,0 +1,8 @@
+﻿$usuario = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).name
+$nomequipo = hostname
+$dominio = systeminfo | findstr /B /C:"Dominio"
+$location = Get-Location
+write-host "Hola $usuario"
+write-host "Tu directorio de trabajo es $location"
+write-host "Perteneces al dominio $dominio"
+write-host "Tu equipo se llama $nomequipo"
